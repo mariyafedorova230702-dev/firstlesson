aa2d844983f27ba7a20936abd03d0d81413681a5
ФедороваМария <mariyafedorova230702@gmail.com>
2025-11-13 00:44:32 +0500

    Python3

---
+++ lessons3.py
@@ -0,0 +1,101 @@
+#1
+
+#text = input('enter text: ')
+#print(text.upper())
+#print(text.lower())
+#print(text.capitalize())
+#print(text.title())
+
+
+#2
+
+#text = input('enter str: ')
+#print(text.isalpha())
+#print(text.isdigit())
+#print(len(text))
+
+
+#3
+
+#text = input('enter text: ')
+#replacement_word = input('enter replacement word: ')
+#new_word = input('inter new word: ')
+#print(text.replace(replacement_word, new_word ))
+#print(text.count(replacement_word))
+
+
+#4
+
+#text = input('enter text with spaces: ')
+#words = text.split()
+#result = '-'.join(words)
+#print(result)
+#print(len(words))
+
+
+#5
+
+#name_file = input('enter name file: ')
+#if name_file.startswith('a') and name_file.lower:
+    #print("There is a letter 'a' in the name")
+#else:
+    #print('There is no letter A in the name') 
+
+
+#if name_file.endswith('.txt'):
+    #print('good')
+#else:
+    #print('should be .txt')           
+
+
+#6
+
+#studets = ['Dima', 'Katya', 'Vova', 'Sonya']
+#print('old list students: ', studets)
+#print('studets: ', len(studets))
+#studets.append('Max')
+#new_student = ['Alex', 'Dias', 'Vanya']
+#studets.extend(new_student)
+#print(studets)
+#print('studets: ', len(studets))
+
+
+#7
+
+#fruits = ['apple', 'banan', 'orange', 'pear', 'banan']
+#print(fruits)
+#print(fruits.index('orange'))
+#print(fruits.count('banan'))
+#fruits.remove('banan')
+#print(fruits)
+
+
+#8
+
+#number = [5, 2, 9, 1, 7, 3]
+#print(number)
+#number.sort()
+#print(number)
+#number.reverse()
+#print(number)
+
+#9
+
+#letter = ['A', 'B', 'D', 'E']
+#print(letter)
+#letter.insert(2, 'C')
+#print(letter)
+#letter.pop(1)
+#print(letter)
+
+
+#10 
+
+text = input('Enter text separated by spaces: ')
+print(text.lower())
+words = text.split()
+words.sort()
+print(words)
+comma = ','.join(words)
+print(comma)
+
